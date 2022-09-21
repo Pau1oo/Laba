c123185580ca9a7559e345e9d1a846b81d37c538
Pau1oo <60032593+Pau1oo@users.noreply.github.com>
2022-09-21 15:38:03 +0300

    Add files via upload

--- 2 лаба/task2.2/task2.2.c
+++ 2 лаба/task2.2/task2.2.c
@@ -4,7 +4,16 @@ int main()
 {
     float C1, C2;
     printf("Input C1 and C2\n");
-    scanf_s("%f %f", &C1, &C2);
+    if (scanf_s("%f %f", &C1, &C2) != 2)
+    {
+        printf("Invalid input");
+        exit();
+    }
+    if ((C1 < 0) || (C2 < 0))
+    {
+        printf("Invalid input");
+        exit();
+    }
     for (int i = 0; i < 6; i++)
     {
         C2 = C2 + C1 / 2;
