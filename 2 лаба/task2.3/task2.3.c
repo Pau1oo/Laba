c2c2af1dafded1f3d20a41af9c7f1d7eff754978
Pau1oo <60032593+Pau1oo@users.noreply.github.com>
2022-09-21 15:39:15 +0300

    Add files via upload

--- 2 лаба/task2.3/task2.3.c
+++ 2 лаба/task2.3/task2.3.c
@@ -2,11 +2,25 @@
 
 int main()
 {
-    unsigned int N, K, temp, count, quotient, rest;
+    int N, K, temp, count, quotient, rest;
     temp = 0;
     count = 0;
     printf("Input N and K (N>K)\n");
-    scanf_s("%u %u", &N, &K);
+    if (scanf_s("%d %d", &N, &K) != 2)
+    {
+        printf("Invalid input");
+        exit();
+    }
+    if (N < K)
+    {
+        printf("Invalid input");
+        exit();
+    }
+    if ((N <= 0) || (K <= 0))
+    {
+        printf("Invalid input");
+        exit();
+    }   
     while (temp <= N)
     {
         temp = temp + K;
